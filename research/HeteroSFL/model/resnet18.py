2452dd00158afb87fac3745264dcbdc08e60b3f2
jiyun98 <gyoon814@kaist.ac.kr>
2023-09-05 01:59:29 +0900

    DepthSFL

---
+++ research/HeteroSFL/model/resnet18.py
@@ -0,0 +1,218 @@
+import math
+import torch
+from torch import nn
+import torch.nn.init as init
+import torch.nn.functional as F
+from math import ceil as up
+from collections import OrderedDict
+
+def _weights_init(m):
+    classname = m.__class__.__name__
+    #print(classname)
+    if isinstance(m, nn.Linear) or isinstance(m, nn.Conv2d):
+        init.kaiming_normal_(m.weight)
+
+
+class LambdaLayer(nn.Module):
+    def __init__(self, lambd):
+        super(LambdaLayer, self).__init__()
+        self.lambd = lambd
+
+    def forward(self, x):
+        return self.lambd(x)
+
+# ---------------------------------------------------
+#                split & pruned resnet
+# ---------------------------------------------------
+class ResNet18_client_v1(nn.Module):  # Dropout (or pruned) ResNet [width] 
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNet18_client_v1, self).__init__()
+        self.in_planes = 64
+        
+        # self.conv1 = nn.Conv2d(3, self.in_planes, kernel_size=7,
+        #                        stride=2, padding=3, bias=False)
+        self.conv1 = nn.Conv2d(3, self.in_planes, kernel_size=3,
+                               stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(self.in_planes)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.apply(_weights_init)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out1 = self.maxpool(F.relu(self.bn1(self.conv1(x))))
+        out2 = self.layer1(out1)
+        out3 = self.layer2(out2)
+        return out3
+    
+
+class ResNet18_server_v1(nn.Module):  # Dropout (or pruned) ResNet [width] 
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNet18_server_v1, self).__init__()
+        self.in_planes = 128
+        
+        # self.conv1 = nn.Conv2d(3, self.in_planes, kernel_size=7,
+        #                        stride=2, padding=3, bias=False)
+        # self.bn1 = nn.BatchNorm2d(self.in_planes)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.linear = nn.Linear(512*block.expansion, num_classes)
+        self.apply(_weights_init)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.layer3(x)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, out.size()[3])
+        out = out.view(out.size(0), -1)
+        logits = self.linear(out)
+        probas = F.softmax(logits, dim=1)
+        return logits, probas
+
+# ---------------------------------------------------
+#                split & pruned resnet v4
+# ---------------------------------------------------
+class ResNet18_client_v2(nn.Module):  # Dropout (or pruned) ResNet [width] 
+    def __init__(self, block, num_blocks,  num_classes=10):
+        super(ResNet18_client_v2, self).__init__()
+        self.in_planes = 64
+        
+        # self.conv1 = nn.Conv2d(3, self.in_planes, kernel_size=7,
+        #                        stride=2, padding=3, bias=False)
+        # self.bn1 = nn.BatchNorm2d(self.in_planes)
+
+        self.conv1 = nn.Conv2d(3, self.in_planes, kernel_size=3,
+                               stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(self.in_planes)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+
+        self.apply(_weights_init)
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out1 = self.maxpool(F.relu(self.bn1(self.conv1(x))))
+        out2 = self.layer1(out1)
+        out3 = self.layer2(out2)
+        out4 = self.layer3(out3)
+        return out4
+    
+
+class ResNet18_server_v2(nn.Module):  # Dropout (or pruned) ResNet [width] 
+    def __init__(self, block, num_blocks,  num_classes=10):
+        super(ResNet18_server_v2, self).__init__()
+        self.in_planes = 256
+        
+        # self.conv1 = nn.Conv2d(3, self.in_planes, kernel_size=7,
+        #                        stride=2, padding=3, bias=False)
+        # self.bn1 = nn.BatchNorm2d(self.in_planes)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.linear = nn.Linear(512*block.expansion, num_classes)
+        self.apply(_weights_init)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.layer4(x)
+        out = F.avg_pool2d(out, out.size()[3])
+        out = out.view(out.size(0), -1)
+        logits = self.linear(out)
+        probas = F.softmax(logits, dim=1)
+        return logits, probas
+
+# ---------------------------------------------------
+#                split & pruned resnet v5
+# ---------------------------------------------------
+class ResNet18_client_v3(nn.Module):  # Dropout (or pruned) ResNet [width] 
+    def __init__(self, block, num_blocks,  num_classes=10):
+        super(ResNet18_client_v3, self).__init__()
+        self.in_planes = 64
+
+        self.conv1 = nn.Conv2d(3, self.in_planes, kernel_size=3,
+                               stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(self.in_planes)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+         # self.bn1 = nn.BatchNorm2d(self.in_planes)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+
+        self.apply(_weights_init)
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out1 = self.maxpool(F.relu(self.bn1(self.conv1(x))))
+        out2 = self.layer1(out1)
+        out3 = self.layer2(out2)
+        out4 = self.layer3(out3)
+        out5 = self.layer4(out4)
+        return out5
+    
+
+class ResNet18_server_v3(nn.Module):  # Dropout (or pruned) ResNet [width] 
+    def __init__(self, block, num_blocks,  num_classes=10):
+        super(ResNet18_server_v3, self).__init__()
+        self.in_planes = 512
+        
+        # self.conv1 = nn.Conv2d(3, self.in_planes, kernel_size=7,
+        #                        stride=2, padding=3, bias=False)
+        # self.bn1 = nn.BatchNorm2d(self.in_planes)
+        layer4 : OrderdDict[str, nn.Module] = OrderedDict()
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], 1, layer4,  1)
+        self.linear = nn.Linear(512*block.expansion, num_classes)
+        self.apply(_weights_init)
+
+    def _make_layer(self, block, planes, num_blocks, stride, Olayer, block_index):
+        strides = [stride] + [1]*(num_blocks-1)
+        ind = block_index
+        # layers = []
+        for stride in strides:
+            Olayer[f"{int(ind)}"] = block(self.in_planes, planes, stride)
+            self.in_planes = planes * block.expansion
+            ind += 1
+        return nn.Sequential(Olayer)
+
+    def forward(self, x):
+        out = self.layer4(x)
+        out = F.avg_pool2d(out, out.size()[3])
+        out = out.view(out.size(0), -1)
+        logits = self.linear(out)
+        probas = F.softmax(logits, dim=1)
+        return logits, probas
+
---
+++ research/HeteroSFL/utils/options.py
@@ -0,0 +1,34 @@
+
+import argparse
+
+def args_parser_main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--num_users', type=int, default=50)
+    parser.add_argument('--noniid', action='store_true') # default: false
+    parser.add_argument('--class_per_each_client', type=int, default=10)
+
+    parser.add_argument('--frac', type=float, default=0.1)
+    parser.add_argument('--bs', type=int, default= 64)
+    parser.add_argument('--local_bs', type=int, default=64)
+    parser.add_argument('--momentum', type=float, default=0)
+    parser.add_argument('--epochs', type=int, default=500)
+    parser.add_argument('--local_ep', type=int, default=5)
+    parser.add_argument('--num_classes', type=int, default=10)
+    parser.add_argument('--lr', type=float, default= 1e-1)
+    parser.add_argument('--weight_decay', type=float, default=0)
+    parser.add_argument('--mode', type=str, default='normal')
+    parser.add_argument('--rs', type=int, default=0)
+
+    parser.add_argument('--model_name', type=str, default='resnet18')
+    parser.add_argument('--data', type=str, default='cifar10')
+    parser.add_argument('--device_id', type=str, default='1')
+    parser.add_argument('--learnable_step', type=bool, default=True)
+
+    parser.add_argument('--run_name',type=str,default = 'vanilla')
+    parser.add_argument('--ps', type = float, default = 1.0)
+    parser.add_argument('--cut_point', default = [])
+    parser.add_argument('--seed', type = int, default = 123)
+    
+    args = parser.parse_args()
+
+    return args
