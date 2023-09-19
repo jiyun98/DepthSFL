2963634ef32da6bd75ff80cec3f2e0ae2b0ab6bd
jiyun98 <gyoon814@kaist.ac.kr>
2023-09-19 11:13:35 +0900

    flop기준

---
+++ DSFL_new_gkt/model/model_DepthSFL/resnet56.py
@@ -0,0 +1,177 @@
+import math
+import torch
+from torch import nn
+import torch.nn.init as init
+import torch.nn.functional as F
+from math import ceil as up
+from collections import OrderedDict
+
+def _weights_init(m):
+    classname = m.__class__.__name__
+    #print(classname)
+    if isinstance(m, nn.Linear) or isinstance(m, nn.Conv2d):
+        init.kaiming_normal_(m.weight)
+
+class LambdaLayer(nn.Module):
+    def __init__(self, lambd):
+        super(LambdaLayer, self).__init__()
+        self.lambd = lambd
+
+    def forward(self, x):
+        return self.lambd(x)
+# ---------------------------------------------------
+#                split & pruned resnet
+# ---------------------------------------------------
+class ResNet56_client_v1(nn.Module): # Dropout (or pruned) ResNet [width] for CIFAR-10
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNet56_client_v1, self).__init__()
+        self.in_planes = 16
+
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3,stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.layer1 = self._make_layer(block, 16, num_blocks[0], stride=1)
+
+        self.apply(_weights_init)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        return out
+
+class ResNet56_server_v1(nn.Module): # Dropout (or pruned) ResNet [width] for CIFAR-10
+    def __init__(self, block, num_blocks,  num_classes=10):
+        super(ResNet56_server_v1, self).__init__()
+        self.in_planes = 16
+
+        self.layer2 = self._make_layer(block, 32, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 64, num_blocks[2], stride=2)
+        self.linear = nn.Linear(64*block.expansion, num_classes)
+
+        self.apply(_weights_init)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.layer2(x)
+        out = self.layer3(out)
+        out = F.avg_pool2d(out, out.size()[3])
+        out = out.view(out.size(0), -1)
+        logits = self.linear(out)
+        probas = F.softmax(logits, dim=1)
+        return logits, probas
+#---------------------------------------------------------------------
+class ResNet56_client_v2(nn.Module): # Dropout (or pruned) ResNet [width] for CIFAR-10
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNet56_client_v2, self).__init__()
+        self.in_planes = 16
+
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3,stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.layer1 = self._make_layer(block, 16, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 32, num_blocks[1], stride=2)
+
+        self.apply(_weights_init)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        return out
+
+class ResNet56_server_v2(nn.Module): # Dropout (or pruned) ResNet [width] for CIFAR-10
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNet56_server_v2, self).__init__()
+        self.in_planes = 32
+        self.layer3 = self._make_layer(block,64, num_blocks[2], stride=2)
+        self.linear = nn.Linear(64*block.expansion, num_classes)
+
+        self.apply(_weights_init)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.layer3(x)
+        out = F.avg_pool2d(out, out.size()[3])
+        out = out.view(out.size(0), -1)
+        logits = self.linear(out)
+        probas = F.softmax(logits, dim=1)
+        return logits, probas
+#----------------------------------------------------------------------
+class ResNet56_client_v3(nn.Module): # Dropout (or pruned) ResNet [width] for CIFAR-10
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNet56_client_v3, self).__init__()
+        self.in_planes = 16
+
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3,stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.layer1 = self._make_layer(block, 16, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 32, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 64, num_blocks[2], stride=2)
+        self.apply(_weights_init)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        return out
+
+class ResNet56_server_v3(nn.Module): # Dropout (or pruned) ResNet [width] for CIFAR-10
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNet56_server_v3, self).__init__()
+        self.in_planes = 64
+
+        self.linear = nn.Linear(64*block.expansion, num_classes)
+
+        self.apply(_weights_init)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.avg_pool2d(x, x.size()[3])
+        out = out.view(out.size(0), -1)
+        logits = self.linear(out)
+        probas = F.softmax(logits, dim=1)
+        return logits, probas
