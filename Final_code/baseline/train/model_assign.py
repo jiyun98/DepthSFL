c70536fa76eb8457d0eb595c01adb451e5b8bbb9
jiyun98 <gyoon814@kaist.ac.kr>
2023-09-30 00:56:45 +0900

    fjord model assign 수정

--- Final_code/baseline/train/model_assign.py
+++ Final_code/baseline/train/model_assign.py
@@ -26,57 +26,35 @@ def FL_model_assignment(cut_point, model_name, device, num_classes = 10):
 def fjord_local_model_assignment(model_name, device, num_classes = 10):
     local_models = []
     if model_name == 'resnet18':
-        ps = [0.1,0.4,0.5,1.0]
+        ps = [0.1,0.53,0.72,1.0]
         for i in range(len(ps)):
             local_models.append(resnet18_f(num_classes, ps[i]).to(device))
     elif model_name == 'resnet34':
-        ps = [0.1,0.4,0.5,1.0]
+        ps = [0.07,0.56,0.74,1.0]
         for i in range(len(ps)):
             local_models.append(resnet34_f(num_classes, ps[i]).to(device))
     elif model_name == 'resnet50':
-        ps = [0.1,0.4,0.5,1.0]
+        ps = [0.07,0.56,0.76,1.0]
         for i in range(len(ps)):
             local_models.append(resnet50_f(num_classes, ps[i]).to(device))
     elif model_name == 'resnet56':
-        ps = [0.1,0.4,0.5,1.0]
+        ps = [0.04,0.54,0.73,1.0]
         for i in range(len(ps)):
             local_models.append(resnet56_f(num_classes, ps[i]).to(device))
     elif model_name == 'resnet101':
-        ps = [0.1,0.4,0.5,1.0]
+        ps = [0.05,0.52,0.74,1.0]
         for i in range(len(ps)):
             local_models.append(resnet101_f(num_classes, ps[i]).to(device))
     elif model_name == 'resnet110':
-        ps = [0.1,0.4,0.5,1.0]
+        ps = [0.1,0.4,0.5,1.0]  # 수정 필요
         for i in range(len(ps)):
             local_models.append(resnet110_f(num_classes, ps[i]).to(device))
 
     for model in local_models:
         model.train()
 
-    return local_models  
+    return local_models , ps
 
-def fjord_global_model_assignment(model_name, device, num_classes = 10):
-    if model_name == 'resnet18':
-        ps = [0.1,0.4,0.5,1.0]
-        glob_model = resnet18_f(num_classes, ps).to(device)
-    elif model_name == 'resnet34':
-        ps = [0.1,0.4,0.5,1.0]
-        glob_model = resnet34_f(num_classes, ps).to(device)
-    elif model_name == 'resnet50':
-        ps = [0.1,0.4,0.5,1.0]
-        glob_model = resnet50_f(num_classes, ps).to(device)
-    elif model_name == 'resnet56':
-        ps = [0.1,0.4,0.5,1.0]
-        glob_model = resnet56_f(num_classes, ps).to(device)
-    elif model_name == 'resnet101':
-        ps = [0.1,0.4,0.5,1.0]
-        glob_model = resnet101_f(num_classes, ps).to(device)
-    elif model_name == 'resnet110':
-        ps = [0.1,0.4,0.5,1.0]
-        glob_model = resnet110_f(num_classes, ps).to(device)
-    glob_model.train()
-
-    return glob_model
 
 #==========================================================================
 '''DetphFL 사용'''
